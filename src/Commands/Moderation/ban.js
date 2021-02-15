3715d45f780daea6694042661fedf8d36053015b
DaatUserName <hopetobylol@gmail.com>
2021-02-15 23:06:16 +0100

    idk

--- src/Commands/Moderation/ban.js
+++ src/Commands/Moderation/ban.js
@@ -1,74 +1,64 @@
 const Command = require('../../Structures/Command');
-const { MessageEmbed } = require('discord.js');
 const banConfig = require('../../database/models/banConfig');
+const { MessageEmbed, MessageAttachment } = require('discord.js');
 
 module.exports = class ban extends Command {
-
     constructor(...args) {
         super(...args, {
-            aliases: ['test1'],
-            description: 'bans the mentioned user of the discord server',
-            category: 'Moderation',
-            guildOnly: true,
-            nsfw: false,
-            PERMISSIONS: ['BAN_MEMBERS']
-        });
+            name: "ban",
+            permission: ['ADMINISTRATOR', 'BAN_MEMBERS'],
+            description: "bans a member from your guild",
+            category: "Moderation",
+            usage: "v!ban @user @reason"
+        })
     }
-
     async run(message, args) {
-        const target = message.mentions.members.first();
+        const member = message.mentions.users.first();
         const reason = args.slice(1).join(" ");
+        const guild = this.client.guilds.cache.get('797204659730907287');
+        if(member.length <  1) return message.reply("You must provide a guild member")
 
-        if(!reason) return message.reply("You must provide a reason");
-        if (target) {
-            
-            const b = new MessageEmbed()
-                .setAuthor(`${target.user.tag}`)
-                .addFields({
-                    name: `Banned member`,
-                    value: `${target.user.tag}`,
-                    inline: true
-                }, {
-                    name: "Banned by:",
-                    value: `${message.author.tag}`,
-                    inline: true
-                }, {
-                    name: "Reason",
-                    value: `${reason}`,
-                    inline: true
-                }, {
-                    name: "Date of punishment",
-                    value: message.createdAt.toLocaleString()
-                }, )
-                .setThumbnail(target.user.displayAvatarURL())
-                .setColor("#008b8b");
+        if(!reason) return message.reply("You must provide a reason to ban!")
 
-            message.channel.send(b);
 
-            const bc = new banConfig({
-                User: target.user.username,
-                UserID: target.id,
-                UserTag: target.user.discriminator,
-                Reason: reason,
-                Duration: "Forever", 
-                Active: true,
-                Moderator: message.author.username,
-                ModeratorTag: message.author.discriminator,
-                ModeratorID: message.author.id,
-            });
-            bc.save();
-
-            await target.send({
+        if(!member.bot) {
+            member.send({
                 embed: {
-                    title: `Dear ${target.user.tag}`,
-                    description: `You have been banned from ${message.guild.name}\n \nBanned by: \n \n${message.author.tag} \n \nreason: ${reason}\n \nDate of punishment: ${message.createdAt.toLocaleString()}`,
-                    color: `#008b8b`
+                    title: `Hello ${member.user.tag}`,
+                    description: `You have been banned in ${guild.name}\nReason: ${reason}\nModerator: ${message.author.tag}\n Moderator id: ${message.author.id}s`,
+                    color: "#ff0000"
                 }
-            }).then(async() => {
-                target.ban(target.id).catch(err => {
-                    console.log(err);
-                });
             });
         }
+
+
+        const bans = new banConfig({
+            User: member.user.username,
+            UserID: member.id,
+            UserTag: member.user.discriminator,
+            Reason: reason,
+            Duration: "Perm", 
+            Active: true,
+            Moderator: message.author.username,
+            ModeratorTag: message.author.discriminator,
+            ModeratorID: message.author.id,
+        });
+
+        bans.save();
+
+        const embed = new MessageEmbed()
+            .setTitle("**VinciMC**")
+            .addFields(
+                { name: "Member ", value: member.user.tag, inline: true},
+                { name: "Reason ", value: reason, inline: true},
+                { name: "Moderator ", value: message.author.tag, inline: false},
+                { name: "Moderator id ", value: message.author.id, inline: false},
+            )
+            .setTimestamp()
+            .setColor("#ff0000")
+            .setFooter(`©️ by ${guild.iconURL()}`)
+            .setThumbnail(member.user.displayAvatarURL({ dynamic: true, format: 'png' }));
+
+        return message.channel.send(embed);
     }
-};
+}

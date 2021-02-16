2d17788077ccecda420e22253b0fa3cc4e320127
DaatUserName <hopetobylol@gmail.com>
2021-02-16 21:13:09 +0100

    idk

---
+++ src/Commands/Admin/scrap.js
@@ -0,0 +1,15 @@
+const Command = require('../../Structures/Command');
+const axoios = require('axios').default;
+
+module.exports = class scrap extends Command {
+    constructor(...args) {
+        super(...args, {
+            name: "scrap",
+            ownerOnly: true
+        })
+    }
+
+    async run(message) {
+        axoios.get('https://cuteondiscord.com').then(res => console.log(res));
+    }
+}

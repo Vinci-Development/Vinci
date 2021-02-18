7e13656a40f178e18eb6c4d6d6a1e9710706a31d
DaatUserName <hopetobylol@gmail.com>
2021-02-18 15:44:31 +0100

    oof

--- src/Commands/Music/play.js
+++ src/Commands/Music/play.js
@@ -19,11 +19,7 @@ module.exports = class play extends Command {
     }
     async run(message, args) {
         if (!message.member.voice.channel) return message.reply("you need to join a voice channel.");
-        if (!args.length) return message.reply("you need to give me a URL or a search term.");
-
-        if (!message.guild.me.hasPermission("CONNECT")) return message.channel.send("I cannot connect to your voice channel, make sure I have permission to!");
-        if (!message.guild.me.hasPermission("SPEAK")) return message.channel.send("I cannot connect to your voice channel, make sure I have permission to!");
-
+        
         const player = this.client.manager.create({
             guild: message.guild.id,
             voiceChannel: message.member.voice.channel.id,

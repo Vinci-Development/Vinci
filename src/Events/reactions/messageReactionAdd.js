0ffbfd8cfffddffe410b0aa9c8517e506667b89f
DaatUserName <hopetobylol@gmail.com>
2021-02-21 22:28:58 +0100

    message reaction add

--- src/Events/reactions/messageReactionAdd.js
+++ src/Events/reactions/messageReactionAdd.js
@@ -1,7 +1,9 @@
-const {
-    urlencoded
-} = require('body-parser');
+const TicketConfig = require('../../database/Mysql/Models/TicketConfig');
+const Ticket = require('../../database/Mysql/Models/Ticket');
+
 const Event = require('../../Structures/Event');
+const { urlencoded } = require('body-parser');
+const { use } = require('random');
 
 module.exports = class messageReactionAdd extends Event {
     constructor(...args) {
@@ -10,6 +12,22 @@ module.exports = class messageReactionAdd extends Event {
         })
     }
     async run(reaction, user) {
-               
+        if(reaction.emoji.name === '🎫') {
+            const config = await TicketConfig.findOne({ where: { messageId: reaction.message.id }});
+            if(config) {
+                const findTicket = await Ticket.findOne({ where: { authorId: user.id, resolved: false }});
+                if(findTicket) user.send('You already have an active ticket... Please resolve it first!');
+                else {
+                    console.log("Creating ticket");
+                    user.send("Creating ticket...");
+                    try {
+                    } catch(err) {
+                        console.log(err)
+                    }
+                }
+            }
+        } else {
+            console.log("No ticket configs found!");
+        }
     }
 }

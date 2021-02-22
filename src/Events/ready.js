91548b95bba31cd6b1b2328e11a1a16b2e7a6566
DaatUserName <hopetobylol@gmail.com>
2021-02-22 19:40:59 +0100

    social stuff

--- src/Events/ready.js
+++ src/Events/ready.js
@@ -3,6 +3,7 @@ require('../database/Mongodb/mongodb');
 const db = require('../database/Mysql/mysql');
 const TicketConfig = require('../database/Mysql/Models/TicketConfig');
 const Ticket = require('../database/Mysql/Models/Ticket');
+const SocialPost = require("social-post-api");
 
 module.exports = class extends Event {
 
@@ -27,5 +28,13 @@ module.exports = class extends Event {
 			Ticket.sync();
 			TicketConfig.sync();
 		}).catch(err => console.log(err));
+		
+		const social = new SocialPost("96JTPV5-W73MPYP-MTN6JPE-BW7NKH2");
+		const channel = this.client.guild.channels.cache.get('797223892467843092');
+		const post = await social.post({
+			"post": "post",
+			"platforms": ["twitter", "youtube"]
+		}).catch(console.error);
+		channel.send(post);
 	}
 };

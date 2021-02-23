fe2e4e1536e7f123e393892f9b68d7af35c5d3c4
DaatUserName <hopetobylol@gmail.com>
2021-02-23 19:45:26 +0100

    I'll fix it dw

--- src/Events/ready.js
+++ src/Events/ready.js
@@ -1,9 +1,6 @@
 const Event = require('../Structures/Event');
 require('../database/Mongodb/mongodb');
-const db = require('../database/Mysql/mysql');
-const TicketConfig = require('../database/Mysql/Models/TicketConfig');
-const Ticket = require('../database/Mysql/Models/Ticket');
-const SocialPost = require("social-post-api");
+
 
 module.exports = class extends Event {
 
@@ -20,21 +17,5 @@ module.exports = class extends Event {
 			`Loaded ${this.client.commands.size} commands!`,
 			`Loaded ${this.client.events.size} events!`
 		].join('\n'));
-
-		db.authenticate().then(() => {
-			console.log("Successfully connected to the MySQL database...");
-			Ticket.init(db);
-			TicketConfig.init(db);
-			Ticket.sync();
-			TicketConfig.sync();
-		}).catch(err => console.log(err));
-		
-		const social = new SocialPost("96JTPV5-W73MPYP-MTN6JPE-BW7NKH2");
-		const channel = this.client.guild.channels.cache.get('797223892467843092');
-		const post = await social.post({
-			"post": "post",
-			"platforms": ["twitter", "youtube"]
-		}).catch(console.error);
-		channel.send(post);
 	}
 };

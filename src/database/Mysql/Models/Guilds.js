289cfc5d069d3d3d4fbedebcfaa2af1167043055
DaatUserName <hopetobylol@gmail.com>
2021-02-24 14:19:41 +0100

    UwU

--- src/database/Mysql/Models/Guilds.js
+++ src/database/Mysql/Models/Guilds.js
@@ -3,7 +3,33 @@ const { DataTypes, Model } = require('sequelize');
 module.exports = class Guilds extends Model {
     static init(sequelize) {
         return super.init({
-            guildId: 
+            guildId: {
+                type: DataTypes.STRING
+            },
+            guildName: {
+                type: DataTypes.STRING
+            },
+            guildOwner: {
+                type: DataTypes.STRING
+            },
+            guildOwnerId: {
+                type: DataTypes.STRING
+            },
+            guildBans: {
+                type: DataTypes.INTEGER
+            },
+            memberSize: {
+                type: DataTypes.INTEGER
+            },
+            roleSize: {
+                type: DataTypes.INTEGER
+            },
+            channelSize: {
+                type: DataTypes.INTEGER
+            }
+        }, {
+            tableName: 'Guilds',
+            sequelize
         })
     }
 }

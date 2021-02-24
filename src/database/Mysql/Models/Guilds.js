999d9bcef17acdc48d0bb953b33913396fd0a520
DaatUserName <hopetobylol@gmail.com>
2021-02-24 14:15:12 +0100

    will be added back

---
+++ src/database/Mysql/Models/Guilds.js
@@ -0,0 +1,9 @@
+const { DataTypes, Model } = require('sequelize');
+
+module.exports = class Guilds extends Model {
+    static init(sequelize) {
+        return super.init({
+            guildId: 
+        })
+    }
+}
